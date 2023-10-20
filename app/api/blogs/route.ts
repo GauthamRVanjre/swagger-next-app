a8a539a28577e15b61330756c05e0eaa79ecf94f
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-20 12:27:42 +0530

    completed GET and POST for blogs

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,3 +1,43 @@
-export const GET = async (req: Request, res: Response) => {};
+import prisma from "@/prisma/prisma";
 
-export const POST = async (req: Request, res: Response) => {};
+type requestBody = {
+  body: string;
+  title: string;
+};
+
+export const GET = async (req: Request, res: Response) => {
+  try {
+    await prisma.$connect();
+    const blogs = await prisma.post.findMany();
+
+    return new Response(JSON.stringify(blogs), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ message: "something went wrong!" }), {
+      status: 500,
+    });
+  } finally {
+    prisma.$disconnect();
+  }
+};
+
+export const POST = async (req: Request, res: Response) => {
+  const requestBody: requestBody = await req.json();
+
+  try {
+    await prisma.$connect();
+    const createPost = await prisma.post.create({
+      data: {
+        title: requestBody.title,
+        body: requestBody.body,
+      },
+    });
+
+    return new Response(JSON.stringify(createPost), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: `${error}` }), {
+      status: 500,
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return <h1 className="text-center">Hello world</h1>;
 }
